b2fe0defc1a3966530c6ad3f191d1b0a2c442cbe
Kainy <kainykawai@gmail.com>
2025-06-20 16:46:07 +0300

    Implement code changes to enhance functionality and improve performance

--- src/app/[locale]/products/car-assistant/page.tsx
+++ src/app/[locale]/products/car-assistant/page.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { motion } from "framer-motion";
-import { useState, useEffect } from "react";
+
+import { useRef } from "react";
 import Image from "next/image";
 
 import TextGradientWhite from "@/components/common/text-gradient-white";
@@ -18,12 +18,24 @@ import {
   VoiceMessageIcon,
 } from "@/components/common/Icons";
 import { useTranslations } from "next-intl";
- 
+import { motion, useScroll, useTransform, useSpring } from "framer-motion";
+import MindsBanner from "@/components/HomePage/minds-meet";
 
 export default function CarAssistantPage() {
-  const [mounted, setMounted] = useState(false);
-
   const t = useTranslations("car-assistant");
+  const d = useTranslations("HomePage");
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "center center"],
+  });
+
+ 
+const scale = useSpring(useTransform(scrollYProgress, [0, 1], [1.5, 0.6]), {
+  stiffness: 100,
+  damping: 30,
+});
+
 
   const features = [
     {
@@ -67,12 +79,6 @@ export default function CarAssistantPage() {
       description: t("reminderDescription"),
     },
   ];
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) return null;
 
   return (
     <div className="min-h-screen bg-background">
@@ -159,8 +165,11 @@ export default function CarAssistantPage() {
         </motion.div>
       </SectionWrapper>
 
-      <SectionWrapper>
-        <div className="container mx-auto flex flex-col-reverse lg:flex-row items-center gap-12 px-4">
+      <SectionWrapper className="relative w-full py-24 px-4 flex justify-center items-center  ">
+        <div
+          ref={ref}
+          className="container mx-auto flex flex-col-reverse lg:flex-row items-center gap-12 px-4"
+        >
           {/* Left Content */}
           <div className="lg:w-1/2 text-center lg:text-left">
             <TextGradientWhite text="Dtec GPT" className="mb-8 text-[90px] " />
@@ -185,11 +194,15 @@ export default function CarAssistantPage() {
         </div>
       </SectionWrapper>
 
-     <SectionWrapper className="w-full    relative  rounded-3xl ">
-    
-     
+      <div className=" py-[128px] relative">
+        <motion.div
+          style={{ scale }}
+          className="absolute -z-0  w-[50vw] h-[50vw] bg-secondary opacity-50 rounded-full blur-[200px] left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2"
+        />
+
+        <SectionWrapper className="w-full relative rounded-3xl ">
           <Image
-            src= "/images/Backgrounds/shape1.svg"
+            src="/images/Backgrounds/shape1.svg"
             alt="Minds Meet Background"
             fill
             className="relative   object-cover bg-black  rounded-3xl  overflow-hidden"
@@ -198,24 +211,64 @@ export default function CarAssistantPage() {
           {/* Content */}
           <div className="relative z-10 text-center">
             <TextGradientWhite
-              text={ t("ComplexQuestionsTitle1")}
+              text={t("ComplexQuestionsTitle1")}
               className="md:text-4xl text-2xl lg:text-[64px] font-bold  text-white"
             />
-                <TextGradientWhite
-              text={ t("ComplexQuestionsTitle2")}
+            <TextGradientWhite
+              text={t("ComplexQuestionsTitle2")}
               className="md:text-4xl text-2xl mb-8 lg:text-[64px] font-bold  text-white"
             />
-      
-       
-  
-          {t("dtecgptDescription")
-            .split("\n")
-            .map((line, i) => (
-              <p className="font-light text-base text-white mb-3 max-w-[720px]" key={i}>{line}</p>
-            ))}
+
+            {t("dtecgptDescription")
+              .split("\n")
+              .map((line, i) => (
+                <p
+                  className="font-light text-base text-white mb-3 max-w-[720px]"
+                  key={i}
+                >
+                  {line}
+                </p>
+              ))}
           </div>
-     
+        </SectionWrapper>
+      </div>
+      <SectionWrapper className="relative w-full bg-transparent py-24 px-4 flex justify-center items-center  ">
+        <div
+          ref={ref}
+          className="container   flex flex-col-reverse lg:flex-row justify-between items-center gap-8 px-4"
+        >
+          {/* Left Content */}
+          <div className="lg:w-1/2 text-center lg:text-left">
+            <TextGradientWhite
+              text="Güvenli ve Verimli Sürüş Deneyimi"
+              className="mb-8 text-[56px] font-black "
+            />
+
+            <p className="font-light text-base max-w-[575px] mx-auto lg:mx-0">
+              Araç Asistanı, manuel dikkat dağıtıcıları ortadan kaldırarak
+              tamamen eller serbest bir kontrol sunar. Kritik uyarıları ve acil
+              durum bildirimlerini sesli olarak iletir; çoklu görev yapma
+              yeteneği sayesinde rota, iletişim veya eğlence işlemlerinizi
+              kesintisiz yönetmenizi sağlar. Böylece hem güvenliğiniz hem de
+              yolculuğunuzun verimliliği en üst düzeye çıkar.
+            </p>
+          </div>
+
+          {/* Right Image */}
+          <div className="lg:w-1/2 flex justify-center">
+            <Image
+              src="/images/Home/dtec-assistants/blue-brain-hologram.png"
+              alt="DtecGPT"
+              width={577}
+              height={387}
+              className=" border-2 relative rounded-2xl  shadow-lg"
+            />
+          </div>
+        </div>
       </SectionWrapper>
+      <MindsBanner text={d("mindsmeet")} />
+
+      {/* Footer Section */}
     </div>
   );
 }
--- src/components/common/SectionWrapper.tsx
+++ src/components/common/SectionWrapper.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 const SectionWrapper = ({children,className}:{
     children: React.ReactNode,
     className?: string
+  
 }) => {
   return (
     <section className={cn("w-full h-full flex items-center justify-center lg:py-[128px] lg:px-4 py-8 px-1  md:py-16 bg-background", className)}>
