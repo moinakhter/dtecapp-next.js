bf71d1e1934b4f8de26b8ad1b8cf398d73ec6a3a
Kainy <kainykawai@gmail.com>
2025-07-15 10:57:10 +0300

    Enhance next.config.ts to include image domain configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import {NextConfig} from 'next';
+import { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
- 
-const nextConfig: NextConfig = {};
- 
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: ['dtecapp-design.vercel.app'],  
+  },
+};
+
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
