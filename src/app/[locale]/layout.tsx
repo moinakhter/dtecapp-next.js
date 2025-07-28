cae4f57deb9e223cc47d30468a68425e3f6c2aba
Kainy <kainykawai@gmail.com>
2025-07-28 15:54:56 +0300

    Feat: changed script in head

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -7,9 +7,10 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { Celias } from "@/lib/utils";
 import Footer from "@/components/Footer";
 import ReactQueryProvider from "./providers";
- 
+ import Script from "next/script";
 import { getTranslations } from "next-intl/server";
 import ShopifyRedirector from "@/components/ShopifyRedirector";
+import Head from "next/head";
 
 
 export async function generateMetadata({ params }: { params: Promise<{ locale: string }> }) {
@@ -54,11 +55,48 @@ export default async function LocaleLayout({
 
   return (
     <html lang={locale} className={Celias.className} suppressHydrationWarning>
+        <Head>
+        {/* Shopify App Bridge script */}
+        <meta name="shopify-api-key" content={process.env.NEXT_PUBLIC_SHOPIFY_API_KEY} />
        
+      </Head>
       <body
         className="bg-background text-foreground font-normal text-[13px] antialiased w-full h-full flex items-center justify-start flex-col overflow-hidden"
         suppressHydrationWarning
       >
+          <Script
+          src="https://cdn.shopify.com/shopifycloud/app-bridge.js"
+          strategy="beforeInteractive"
+        />
+        <Script
+          id="shopify-app-bridge-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function () {
+                const params = new URLSearchParams(window.location.search);
+                const shop = params.get("shop");
+                const host = params.get("host");
+
+                if (window.ShopifyAppBridge && shop && host) {
+                  const AppBridge = window.ShopifyAppBridge;
+                  const createApp = AppBridge.default;
+                  const actions = AppBridge.actions;
+                  const Redirect = actions.Redirect;
+
+                  const app = createApp({
+                    apiKey: '${process.env.NEXT_PUBLIC_SHOPIFY_API_KEY}',
+                    host: host,
+                    forceRedirect: true
+                  });
+
+                  const redirect = Redirect.create(app);
+                  redirect.dispatch(Redirect.Action.ADMIN_PATH, "/settings/general");
+                }
+              })();
+            `,
+          }}
+        />
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
